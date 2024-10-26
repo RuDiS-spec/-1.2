5b375999ab026b51ef06e5cc7188e7d9db6c3e5d
RuDiS-spec <dmitrijrunda508@gmail.com>
2024-10-26 21:20:22 +0300

    Add files via upload

---
+++ модуль1.2.py
@@ -0,0 +1,5 @@
+quantity_completed_homeworks = 12
+quantity_hours_spent = 1.5
+name_of_course = 'Python'
+time_for_one_task = 0.125
+print(name_of_course,quantity_completed_homeworks,quantity_hours_spent,time_for_one_task )
